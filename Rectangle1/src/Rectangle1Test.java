c2d8a4db66a5b95f760437ad32d773e7afab5154
SilentDreamz <ddeep21@vt.edu>
2022-09-10 15:01:34 -0400

    Rectangle1 test start changes

--- Rectangle1/src/Rectangle1Test.java
+++ Rectangle1/src/Rectangle1Test.java
@@ -1,31 +1,35 @@
+
 /**
  * 
  */
 import student.TestCase;
 import student.testingsupport.PrintStreamWithHistory;
 import java.util.NoSuchElementException;
-// May need more imports for file exceptions,etc
 
 /**
- * @author Deep Datta, (name here)
+ * @author Deep Datta (PID: ddeep21), Enk Naran (PID: enk)
  * Note: make sure to write comments and concerns later
  */
 public class Rectangle1Test extends TestCase{
 
     // use systemOut().getHistory()
     // look up "test main" in Piazza and find related posts on how to test main
-    
+    // Note: need to do sysoout.clearHistory() to clear it
     private Rectangle1 mian;
     
     public void setUp() {
         
         mian = new Rectangle1();
-        // How to test main class?
+        mian.toString();
+        
         
     }
     
     public void testParse() {
         
+        String testInput = "test.txt";
+        mian.parse(testInput);
+        assertTrue(systemOut().getHistory().equals(""));
         
     }
 }
